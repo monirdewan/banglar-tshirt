49994af002da403d8a2ab7091a45324c8318adbe
Monir <hossinmonir681@gmail.com>
2023-04-10 10:02:37 +0600

    initial data load

---
+++ src/components/OrderRview/OrderRview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderRview = () => {
+    return (
+        <div>
+            <h2>This is order review Page</h2>
+        </div>
+    );
+};
+
+export default OrderRview;
